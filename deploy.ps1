792da44b675a07840db001c402524b5169a07160
C.Rud Glasdam <cecilie.rud@hotmail.com>
2026-03-10 15:07:38 +0100

    Opdater deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,8 @@
+# Gem deploy.ps1 i master først
+git add deploy.ps1
+git commit -m "Opdater deploy script"
+git push origin master
+
 # Byg projektet
 cd ponyoclicker
 dotnet publish -c Release
@@ -9,12 +14,17 @@ git checkout gh-pages
 # Kopier filer
 xcopy /E /Y ponyoclicker\bin\Release\net9.0\publish\wwwroot\* .
 
-# Ret base href
+# Opret 404.html hvis den ikke findes
+if (-not (Test-Path "404.html")) {
+    Copy-Item index.html 404.html
+}
+
+# Ret base href i begge filer
 (Get-Content index.html) -replace '<base href="/" />', '<base href="/ponyoclicker/" />' | Set-Content index.html
 (Get-Content 404.html) -replace '<base href="/" />', '<base href="/ponyoclicker/" />' | Set-Content 404.html
 
 # Push
-git add -A
+git add _framework css images index.html 404.html ponyoclicker.styles.css
 git commit -m "Deploy"
 git push origin gh-pages
 
